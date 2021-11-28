47415d30ae8a7ec82f7a3075e932d53dc3a6d2d6
Xuefeng Zhu <xzhu15@illinois.edu>
2021-11-28 03:34:00 -0800

    add like and dislike

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
+import _ from 'lodash';
 import { Typography, Box } from '@mui/material';
 import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
 import ReactPlayer from 'react-player';
-// import Moralis from 'moralis';
 import { useMoralis } from 'react-moralis';
 
 import { useStateContext } from '../contexts/StateContextProvider';
-import VideoItem from './VideoItem';
 import Loader from './Loader';
 
 const VideoDetail = () => {
   const { id } = useParams();
-  const { Moralis, isInitialized } = useMoralis();
+  const { Moralis, isInitialized, account } = useMoralis();
   const { retrieveNFTDetails } = useStateContext();
   const [videoStats, setVideoStats] = useState(null);
+  const [likes, setLikes] = useState([]);
+  const [dislikes, setDislikes] = useState([]);
   const { isLoading, data } = useQuery(['NFTDetails', id], () =>
     retrieveNFTDetails(id)
   );
@@ -37,19 +38,58 @@ const VideoDetail = () => {
     } else {
       vs = new VideoStats();
       vs.set('nftId', id);
+      vs.set('likes', []);
+      vs.set('dislikes', []);
       vs.set('views', 0);
     }
 
     vs.set('views', vs.get('views') + 1);
     await vs.save();
     setVideoStats(vs);
-    console.log(vs.get('views'));
+    setLikes(vs.get('likes'));
+    setDislikes(vs.get('dislikes'));
   }, [id, isInitialized]);
 
   const videoDetail = data?.nft;
   const videoSrc = videoDetail?.metadata.playbackId
     ? `https://cdn.livepeer.com/hls/${videoDetail?.metadata.playbackId}/index.m3u8`
     : videoDetail?.metadata.external_url;
+
+  function likeVideo() {
+    if (!account) {
+      alert('Please connect wallet to perform this action');
+      return;
+    }
+
+    let newLikes;
+    if (_.includes(likes, account)) {
+      newLikes = _.remove(likes, account);
+    } else {
+      newLikes = [...likes, account];
+    }
+
+    videoStats.set('likes', newLikes);
+    videoStats.save();
+    setLikes(newLikes);
+  }
+
+  function dislikeVideo() {
+    if (!account) {
+      alert('Please connect wallet to perform this action');
+      return;
+    }
+
+    let newDislikes;
+    if (_.includes(dislikes, account)) {
+      newDislikes = _.remove(dislikes, account);
+    } else {
+      newDislikes = [...dislikes, account];
+    }
+
+    videoStats.set('dislikes', newDislikes);
+    videoStats.save();
+    setDislikes(newDislikes);
+  }
 
   if (isLoading) {
     return <Loader />;
@@ -87,48 +127,51 @@ const VideoDetail = () => {
               </Typography>
             </Box>
 
-            {/* <Box
+            <Box
+              sx={{
+                opacity: 0.7,
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 3,
+              }}
+              className="like-dislike"
+            >
+              <Typography
                 sx={{
-                  opacity: 0.7,
+                  marginBottom: '5px',
                   display: 'flex',
-                  flexWrap: 'wrap',
-                  gap: 3,
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                  gap: 1,
+                  cursor: 'pointer',
                 }}
-                className="like-dislike"
+                onClick={likeVideo}
               >
-                <Typography
-                  sx={{
-                    marginBottom: '5px',
-                    display: 'flex',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    gap: 1,
-                  }}
-                >
-                  <ThumbUpAltOutlinedIcon />
-                  <Typography>
-                    {parseInt(
-                      videoDetail?.statistics?.likeCount
-                    ).toLocaleString('en-US')}
-                  </Typography>
+                <ThumbUpAltOutlinedIcon
+                  color={_.includes(likes, account) ? 'primary' : ''}
+                />
+                <Typography>
+                  {parseInt(likes.length).toLocaleString('en-US')}
                 </Typography>
-                <Typography
-                  sx={{
-                    marginBottom: '5px',
-                    display: 'flex',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    gap: 1,
-                  }}
-                >
-                  <ThumbDownAltOutlinedIcon />
-                  <Typography>
-                    {parseInt(
-                      videoDetail?.statistics?.dislikeCount
-                    ).toLocaleString('en-US')}
-                  </Typography>
+              </Typography>
+              <Typography
+                sx={{
+                  marginBottom: '5px',
+                  display: 'flex',
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                  gap: 1,
+                }}
+                onClick={dislikeVideo}
+              >
+                <ThumbDownAltOutlinedIcon
+                  color={_.includes(dislikes, account) ? 'primary' : ''}
+                />
+                <Typography>
+                  {parseInt(dislikes.length).toLocaleString('en-US')}
                 </Typography>
-              </Box>*/}
+              </Typography>
+            </Box>
           </Box>
           <Typography>{videoDetail?.metadata.description}</Typography>
         </Box>
